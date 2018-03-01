e9068061c288b6587025d2990ad32049c744dccf
Matthew Meehan <matthew.meehan@intergen.co.nz>
2018-03-02 08:53:14 +1300

    updated test to conform to tslint style

--- test.ts
+++ test.ts
@@ -1,29 +1,39 @@
-/// <reference path="./index.d.ts" />
-SP.SOD.import(['sp.js'])
-.then(() => {
-    var context = new SP.ClientContext();
-    var web = context.get_web();
-    var lists = web.get_lists();
-    var list = lists.getByTitle('Documents');
-    var items: SP.ListItemCollection = list.get_queryResult("<View></View>");
+/// <reference path="./src/index.d.ts" />
+SP.SOD.import(['sp.js']).then(() => {
+    const context = new SP.ClientContext();
+    const web = context.get_web();
+    const lists = web.get_lists();
+    const list = lists.getByTitle('Documents');
+    const items: SP.ListItemCollection = list.get_queryResult('<View></View>');
     context.load(items);
-    context.executeQuery()
-    .then(() => {
-        items.each((i, item) => { item.get_item('Title'); });
-        items.every((item, i, items) => item.get_item('Title') == "") == false;
-        items.find((item, i, items) => item.get_item('Title') == "").get_item('Title');
-        var filtered1: SP.ListItem[] = items.filter(item => item.get_item('Title') == "");
-        var filtered2: SP.ListItem[] = items.filter({"Title": "", "Author": web.get_currentUser()});
+    context.executeQuery().then(() => {
+        items.each((i, item) => {
+            item.get_item('Title');
+        });
+        items.every((item, i, items) => item.get_item('Title') === '') ===
+            false;
+        items
+            .find((item, i, items) => item.get_item('Title') === '')
+            .get_item('Title');
+        const filtered1: SP.ListItem[] = items.filter(
+            item => item.get_item('Title') === ''
+        );
+        const filtered2: SP.ListItem[] = items.filter({
+            Title: '',
+            Author: web.get_currentUser()
+        });
         items.firstOrDefault().get_item('Title');
         items.map((item, i) => item.get_item('Title')).length;
-        items.some((item, i, items) => item.get_item('Title') == "") == false;
+        items.some((item, i, items) => item.get_item('Title') === '') === false;
         items.toArray()[0].get_item('Title');
-        var groups : {[group:string]:SP.ListItem[]} = items.groupBy(function(item) {
-            return item.get_item('ContentType');
-        });
-        var total_sum: number = items.reduce(function(sum, item) {
+        const groups: { [group: string]: SP.ListItem[] } = items.groupBy(
+            item => {
+                return item.get_item('ContentType');
+            }
+        );
+        const total_sum: number = items.reduce((sum, item) => {
             return sum + item.get_item('Total');
-        }, 0);
-    })
-    var guid = SP.Guid.generateGuid();
+        },                                     0);
+    });
+    const guid = SP.Guid.generateGuid();
 });
